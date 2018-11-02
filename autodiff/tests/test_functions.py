75e58757234d0c3ebba7e6ba65c8b396dafb5839
Bhaven Patel <heybhaven@yahoo.com>
2018-11-02 18:16:29 -0400

    fixed errors in test_functions.py

--- autodiff/tests/test_functions.py
+++ autodiff/tests/test_functions.py
@@ -9,19 +9,19 @@ import autodiff as ad
 def test_sin():
     
     x = ad.Scalar('x', 2)
-    val = sin(x)
+    val = ad.sin(x)
 
     assert (np.isclose(val.getValue(),0.90929742682)==True)
     assert (np.isclose(val.getDeriv()['x'],-0.41614683654)==True)
 
     x = ad.Scalar('x', -2)
-    val = sin(x)
+    val = ad.sin(x)
 
     assert (np.isclose(val.getValue(),-0.9092974268256817)==True)
     assert (np.isclose(val.getDeriv()['x'],-0.4161468365471424)==True)
 
     x = ad.Scalar('x', 0)
-    val = sin(x)
+    val = ad.sin(x)
 
     assert (np.isclose(val.getValue(),0.0)==True)
     assert (np.isclose(val.getDeriv()['x'],1.0)==True)
@@ -29,19 +29,19 @@ def test_sin():
 def test_cos():
     
     x = ad.Scalar('x', 2)
-    val = cos(x)
+    val = ad.cos(x)
 
     assert (np.isclose(val.getValue(),-0.41614683654)==True)
     assert (np.isclose(val.getDeriv()['x'],-0.9092974268256817)==True)
 
     x = ad.Scalar('x', -2)
-    val = cos(x)
+    val = ad.cos(x)
 
     assert (np.isclose(val.getValue(),-0.4161468365471424)==True)
     assert (np.isclose(val.getDeriv()['x'],0.9092974268256817)==True)
 
     x = ad.Scalar('x', 0)
-    val = cos(x)
+    val = ad.cos(x)
 
     assert (np.isclose(val.getValue(),1.0)==True)
     assert (np.isclose(val.getDeriv()['x'],0.0)==True)
