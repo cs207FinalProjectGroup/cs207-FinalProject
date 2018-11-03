b039e5be333643bcacd6d35a8e7907bffb05ca16
Julien Laasri <jlaasri@g.harvard.edu>
2018-11-03 15:23:15 -0400

    Added exp, tan and power imports in init file

--- autodiff/__init__.py
+++ autodiff/__init__.py
@@ -2,3 +2,6 @@ from autodiff.scalar import Scalar
 from autodiff.vector import Vector
 from autodiff.functions import cos
 from autodiff.functions import sin
+from autodiff.functions import exp
+from autodiff.functions import tan
+from autodiff.functions import power
