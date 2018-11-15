e9c6e9a7aa105da06c43a9260a731c2065342da4
Bhaven Patel <3346052+bhavenp@users.noreply.github.com>
2018-11-15 13:09:23 -0500

    basic tests of sqrt work

--- autodiff/__init__.py
+++ autodiff/__init__.py
@@ -1,6 +1,2 @@
 from autodiff.scalar import Scalar
-#from autodiff.functions import cos
-#from autodiff.functions import sin
-#from autodiff.functions import exp
-#from autodiff.functions import tan
 from autodiff.functions import *
